f7e977f88bdb9649e4a8b359eaee86aad5579b1d
Schmowser <victor.warno@googlemail.com>
2019-03-29 17:02:16 +0100

    Corrected dependencies

--- src/main/java/de/schmowser/radio/RadioRepetitiveApplication.java
+++ src/main/java/de/schmowser/radio/RadioRepetitiveApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class RadioRepetitiveApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args){
+
         SpringApplication.run(RadioRepetitiveApplication.class, args);
+
     }
+
 }
